107585c70a213c11620e2401afa32236554389f4
Greetje Jongen <greetje.jongen@ucll.be>
2019-05-06 10:01:00 +0200

    formulier aangepast

--- src/Controller.java
+++ src/Controller.java
@@ -7,25 +7,26 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     public Controller() {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// als boolean false dan foute manier
-		// als boolean true dan juiste manier (PRG)
-    	boolean loggedIn = true;
-		if (loggedIn) {
-			response.sendRedirect("ok.jsp");
-		}
-		else {
-			request.getRequestDispatcher("ok.jsp").forward(request, response);
-		}
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // als boolean false dan foute manier
+        // als boolean true dan juiste manier (PRG)
+        boolean withPRG = false;
+        if (request.getParameter("prg") != null && request.getParameter("prg").equals("true"))
+            withPRG = true;
+        if (withPRG) {
+            response.sendRedirect("ok.jsp");
+        } else {
+            request.getRequestDispatcher("ok.jsp").forward(request, response);
+        }
+    }
 
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,27 +1,27 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+         pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Countries</title>
+    <meta charset="UTF-8">
+    <title>Countries</title>
 </head>
 <body>
-	<main id="container">
-	<article>
-		<h1>Login</h1>
-		<form method="POST" action="Controller">
-				<p>
-					<label for="name">Name: </label>
-					<input id="name" name="name" type="text">
-				</p>
-				<p>
-					<label for="password">Password: </label>
-					<input id="password" name="password" type="password">
-				</p>
-				<input id="bewaar" type="submit" value="Save">
-		</form>
-	</article>
-	</main>
+<main id="container">
+    <article>
+        <h1>Login</h1>
+        <form method="POST" action="Controller">
+            <p>
+                <input type="radio" id="withPRG" name="prg" value="true" checked>
+                <label for="withPRG">With PRG</label>
+            </p>
+            <p>
+                <input type="radio" id="withoutPRG" name="prg" value="false">
+                <label for="withoutPRG">Without PRG</label>
+            </p>
+            <input id="save" type="submit" value="Save">
+        </form>
+    </article>
+</main>
 </body>
 </html>
